786a6833827e560fb978c831d55637b7a7ffe3ea
Pragati Poudel <pragati.poudel37@gmail.com>
2022-11-30 10:05:09 -0500

    Draw circles and crosses

--- game.js
+++ game.js
@@ -5,7 +5,9 @@ const ctx = canvas.getContext("2d");
 document.addEventListener('click', mouseClick);
 
 function mouseClick(e) {
-
+    let x = e.clientX - canvas.offsetLeft - linePadding;
+    let y = e.clientY - canvas.offsetTop - linePadding;
+    positionDet(x, y);
 }
 
 
@@ -13,6 +15,8 @@ function mouseClick(e) {
 const linePadding = 20;
 const lineSpacing = (canvas.width - linePadding*2)/3;
 const lineLength = canvas.width - linePadding*2;
+
+let circle = true;
 
 
 
@@ -42,5 +46,41 @@ function drawLine() {
 }
 
 
+function positionDet(x, y) {
+        for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if ((x > j * lineSpacing) && (x < (j+1) * lineSpacing) && (y > i * lineSpacing) && (y < (i+1) * lineSpacing)) {
+                if (circle) {
+                    drawCircle(i, j);
+                } else {
+                    drawCross(i, j);
+                }
+            }
+        }
+    }
+}
+
+function drawCircle(i, j) {
+    ctx.beginPath();
+    let a = linePadding + lineSpacing / 2 + lineSpacing * j;
+    let b = linePadding + lineSpacing / 2 + lineSpacing * i;
+    ctx.arc(a, b, 20, 0, Math.PI*2);
+    ctx.fillStyle = "Black";
+    ctx.stroke();
+    ctx.closePath();
+    circle = false;
+}
+
+function drawCross(i, j) {
+    ctx.beginPath();
+    let a = linePadding + (lineSpacing / 2) + lineSpacing * j;
+    let b = linePadding + (lineSpacing/2) + lineSpacing * i;
+    ctx.moveTo( a - 20, b - 20);
+    ctx.lineTo(a + 20, b + 20);
+    ctx.moveTo(a + 20, b - 20);
+    ctx.lineTo(a - 20, b + 20);
+    ctx.stroke();
+    circle = true;
+}
 
 drawLine();
